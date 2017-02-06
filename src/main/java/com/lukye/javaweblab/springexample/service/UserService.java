7e2a05120f7e2fc9e0ef17dc18b4fdd98dfa8d04
lukye <lujian0517@163.com>
2017-02-06 16:26:40 +0800

    Add test

--- src/main/java/com/lukye/javaweblab/springexample/service/UserService.java
+++ src/main/java/com/lukye/javaweblab/springexample/service/UserService.java
@@ -2,13 +2,16 @@ package com.lukye.javaweblab.springexample.service;
 
 import com.lukye.javaweblab.springexample.mapper.UserMapper;
 import com.lukye.javaweblab.springexample.model.User;
-import org.apache.ibatis.binding.MapperProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private UserMapper mapper;
+
+    public UserService()
+    {
+    }
 
     @Autowired
     public UserService(UserMapper mapper)
---
+++ src/test/java/com/lukye/javaweblab/springexample/UserServiceTest.java
@@ -0,0 +1,26 @@
+package com.lukye.javaweblab.springexample;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/spring.xml" })
+public class UserServiceTest {
+    public UserServiceTest()
+    {
+    }
+
+    @Before
+    public void setUp()
+    {
+    }
+
+    @Test
+    public void testInsert()
+    {
+
+    }
+}
