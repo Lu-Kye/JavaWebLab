64183bb906bf4a9caaf1bbcf8f6ab9b1fcc46ac7
lukye <lujian0517@163.com>
2017-02-06 10:17:11 +0800

    Add insert method in UserService

--- src/main/java/com/lukye/javaweblab/springexample/service/UserService.java
+++ src/main/java/com/lukye/javaweblab/springexample/service/UserService.java
@@ -1,10 +1,23 @@
 package com.lukye.javaweblab.springexample.service;
 
+import com.lukye.javaweblab.springexample.mapper.UserMapper;
+import com.lukye.javaweblab.springexample.model.User;
 import org.apache.ibatis.binding.MapperProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
-    public void Test()
-    {
+    private UserMapper mapper;
 
+    @Autowired
+    public UserService(UserMapper mapper)
+    {
+        this.mapper = mapper;
+    }
+
+    public void insert(User user)
+    {
+        mapper.insert(user);
     }
 }
