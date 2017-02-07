ea03ba40fe300a24a4e4614e03b17a5088025dcc
lukye <lujian0517@163.com>
2017-02-07 15:07:16 +0800

    Add insert user test

--- src/main/java/com/lukye/javaweblab/springexample/service/UserService.java
+++ src/main/java/com/lukye/javaweblab/springexample/service/UserService.java
@@ -5,6 +5,9 @@ import com.lukye.javaweblab.springexample.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.LongSummaryStatistics;
+import java.util.StringJoiner;
+
 @Service
 public class UserService {
     private UserMapper mapper;
@@ -13,6 +16,7 @@ public class UserService {
     {
     }
 
+    @Autowired
     public UserService(UserMapper mapper)
     {
         this.mapper = mapper;
@@ -21,5 +25,15 @@ public class UserService {
     public void insert(User user)
     {
         mapper.insert(user);
+    }
+
+    public void delete(String name)
+    {
+        mapper.deleteByPrimaryKey(name);
+    }
+
+    public User select(String name)
+    {
+        return mapper.selectByPrimaryKey(name);
     }
 }
--- src/test/java/com/lukye/javaweblab/springexample/UserServiceTest.java
+++ src/test/java/com/lukye/javaweblab/springexample/UserServiceTest.java
@@ -1,14 +1,27 @@
 package com.lukye.javaweblab.springexample;
 
+import com.lukye.javaweblab.springexample.model.User;
+import com.lukye.javaweblab.springexample.service.UserService;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Date;
+import java.util.StringJoiner;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"/context.xml"})
+@ContextConfiguration(locations = {"classpath:context.xml"})
 public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    private final String NAME = "root";
+    private Long id;
+
     public UserServiceTest()
     {
     }
@@ -21,6 +34,28 @@ public class UserServiceTest {
     @Test
     public void testInsert()
     {
+        User user = new User();
+        user.setName("root");
+        user.setPassword("root");
+        user.setBirthday(new Date());
+        user.setCreated(new Date());
+        user.setUpdated(new Date());
+        user.setUsername("Test Insert");
+        userService.insert(user);
 
+        Assert.assertNotNull(userService.select(NAME));
+    }
+
+    @Test
+    public void testSelect()
+    {
+        User user = userService.select(NAME);
+    }
+
+    @Test
+    public void testDelete()
+    {
+        User user = userService.select(NAME);
+        userService.delete(user.getName());
     }
 }
